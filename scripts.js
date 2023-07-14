ed3e42cb55bd3ea33301fe65276098942ec08b3a
Nidia Menchaca <nmenchaca@nidias-air.lan>
2023-07-13 20:30:32 -0500

    status

--- scripts.js
+++ scripts.js
@@ -1,2 +1,22 @@
 // Write your JavaScript code here.
 // Remember to pay attention to page loading!
+
+function init() {
+    let takeOffButton = document.getElementById("takeoff");
+    let status = document.getElementById("flightStatus");
+    let background = document.getElementById("shuttleBackground");
+    
+    
+   
+    takeOffButton.addEventListener("click", function (event) {
+        console.log("confirms that shuttle is ready to takeoff!");
+        status.innerHTML = ("Shuttle In Flight")
+        background.style.backgroundColor = "blue";
+    });
+
+    takeOffButton
+
+
+
+} window.addEventListener('load', init)   
+
